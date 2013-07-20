6537f77c45584ec1c5fd1ba66487e9530aacb30a
Ryan Tablada <ryan.tablada@gmail.com>
2013-07-20 05:16:35 -0400

    Starting to work on token grabber

---
+++ src/Rtablada/StripeConnect/StripeConnect.php
@@ -0,0 +1,35 @@
+<?php namespace Rtablada\StripeConnect;
+
+use Rtablada\SlimPost\SlimPost;
+use Illuminate\Config\Repository as Config;
+
+class StripeConnect
+{
+	protected $config;
+	protected $attributes;
+	protected $url = 'https://connect.stripe.com/oauth/token';
+
+	public function __construct(Config $config)
+	{
+		$this->attributes = array(
+			'client_secret' => $this->config->get('stripe.app_client_id'),
+			'grant_type' => 'authorization_code',
+		);
+	}
+
+	public function setCode($code)
+	{
+		$this->attributes['code'] = $code;
+	}
+
+	public function getToken()
+	{
+		$poster = new SlimPost()
+	}
+
+	public function getTokenWithCode($code)
+	{
+		$this->setCode($code);
+		return $this->getToken();
+	}
+}
